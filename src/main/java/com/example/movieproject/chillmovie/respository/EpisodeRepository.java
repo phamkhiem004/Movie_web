2137ab6db4037cdf9004974ac9e2fd41b025c6c5
Pham Khiem <tula5904@gmail.com>
2026-04-25 10:40:03 +0700

    add repo component for episode

--- src/main/java/com/example/movieproject/chillmovie/respository/EpisodeRepository.java
+++ src/main/java/com/example/movieproject/chillmovie/respository/EpisodeRepository.java
@@ -2,9 +2,11 @@ package com.example.movieproject.chillmovie.respository;
 
 import com.example.movieproject.chillmovie.entity.Episode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EpisodeRepository extends JpaRepository<Episode,Integer> {
 
     // Lấy danh sách tập theo movie
